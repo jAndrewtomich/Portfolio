846a070c505999ee26ca20751a88d237d089d09b
andrew <johnatomich@gmail.com>
2021-10-28 16:16:59 -0500

    Add logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,45 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
     
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/portfolio.log',
+                    maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Portfolio startup')
+    
     return app
 
+
+from app import models
