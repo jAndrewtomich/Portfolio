85a0edeebbb9797b5df19d854597eeedd1cc1b0f
andrew <johnatomich@gmail.com>
2021-10-28 16:13:57 -0500

    Add log_to_stdout

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mum'
-    SQL_ALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = bool(int(os.environ.get('LOG_TO_STDOUT')))
+
