3e0cb5ca9b7bed307675b5a1bf6f25c017c7482e
andrew <johnatomich@gmail.com>
2021-10-30 16:30:31 -0500

    Create unit test file tests.py

---
+++ tests.py
@@ -0,0 +1,23 @@
+#!/home/andrew/anaconda3/envs/mega/bin/python
+import unittest
+from app import create_app, db
+from app.models import Eda
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+
+class EdaBlockCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
