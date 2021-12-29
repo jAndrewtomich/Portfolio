32d140c848b70e5a07d5e58623290a48e3475263
jatomic <johnatomich@gmail.com>
2021-12-29 08:04:40 -0600

    Update flask shell context

--- portfolio.py
+++ portfolio.py
@@ -1,8 +1,8 @@
 from app import create_app, db
-from app.models import Eda
+from app.models import Topic
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Eda': Eda}
+    return {'db': db, 'topic': Topic}
