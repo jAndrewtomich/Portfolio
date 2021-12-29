1040acfce92f63db8459e219fd4bce3d486fc48a
jatomic <johnatomich@gmail.com>
2021-12-29 09:47:51 -0600

    Update config

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mum'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        "postgres://", "postgresql://") or \
-        "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = bool(int(os.environ.get('LOG_TO_STDOUT')))
