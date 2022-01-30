6b8d64812438659b040e657eca47c807459a582f
jatomic <johnatomich@gmail.com>
2022-01-29 22:35:50 -0600

    Update settings config

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = bool(int(os.environ.get('LOG_TO_STDOUT')))
-
