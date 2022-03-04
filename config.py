e9769ff73681c261f2062270523524f3b2589291
jatomic <johnatomich@gmail.com>
2022-03-04 13:37:46 -0600

    Heroku fix 10

--- config.py
+++ config.py
@@ -4,4 +4,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mum'
-    LOG_TO_STDOUT = bool(int(os.environ.get('LOG_TO_STDOUT')))
