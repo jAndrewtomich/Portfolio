87d99b46242e05e67041b8f3de752d16e2355d7d
jatomic <johnatomich@gmail.com>
2022-03-04 13:33:32 -0600

    Refactor to single page app

--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,25 @@
-from app import create_app
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from config import Config
 
-app = create_app()
+app = Flask(__name__)
+Bootstrap(app)
+app.config.from_object(Config)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title="Andrew's Page")
+
+@app.route('/eda')
+def eda():
+    return render_template('eda.html', title="Netflix and IMDb EDA")
+
+@app.route('/flights')
+def flights():
+    return render_template('flights.html', title="Flight Notifications")
+
+
+@app.route('/hn')
+def hn():
+    return render_template('hackernews.html', title="News Summarization")
